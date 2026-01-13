14a43ea90e9874e2779682984163ebcad0048e1c
SANZIDA10 <sanzidajerin28@gmail.com>
2026-01-13 19:13:01 +0600

    update database and other files

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,8 @@ module com.example.myapp {
     requires javafx.graphics;
     requires java.sql;
 
+    requires javafx.base;
+
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
